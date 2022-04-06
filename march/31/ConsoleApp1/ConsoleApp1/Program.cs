d40cc8ce70ba958958ab9afa3b5620b74920a395
= <=>
2022-04-06 22:54:43 +0200

    hometasks

--- march/31/ConsoleApp1/ConsoleApp1/Program.cs
+++ march/31/ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,137 +11,91 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
-            //Homework 1:
-            // 5 dənə dəyişəni Implicit Conversion,
-            {
-                int i1 = 5;
-                long i1ToLong = i1;
-
-                short i2 = 12;
-                int i2ToInt = i2;
-
-                float i3 = 12.5F;
-                double i3ToDouble = i3;
+            //int bignum = 5466465;
+            //byte iiiToByte2 = (byte)bignum;
+            //byte iiiToByte = Convert.ToByte(bignum);
 
 
-                char i4 = 'a';
-                long i4ToLong = i4;
+            //Task 1
 
-                short i5 = 3254;
-                long i5ToLong = i5;
+            int i = 5;
+            byte b = 1;
+            float f = 5.5F;
+            long l = 654644564656;
+            short s = 2313;
+            string st = "465";
+            string st1 = "h";
+            char c = '5';
 
-            }
-            // 5 dənə dəyişəni Explicit Conversion,
-            {
-                long i1 = 5;
-                int i1ToInt = (int)i1;
+            //Implicit
+            long intToLong = i;
+            short byteToShort = b;
+            double floatToDouble = f;
+            float longToFloat = l;
+            decimal shortToDecimal = s;
 
-                short i1ToShort = (short)i1;
+            //Explicit
 
-                decimal i3 = 12.5M;
-                double i3ToDouble = (double)i3;
+            short longToShort = (short)l;
+            decimal floatToDecimal = (decimal)f;
+            byte intToByte = (byte)i;
+            int longToInt = (int)l;
+            byte floatToByte = (byte)f;
 
+            //Helper
 
-                char i4 = 'a';
-                long i4ToLong = i4;
-
-                short i5 = 3254;
-                long i5ToLong = i5;
-            }
-            // 5 dənə dəyişəni Helper Class Conversion vasitəsilə convert edin. Dəyişən tiplərini və dəyərlərini özünüz seçin.
-
-
-
-
-            //  Homework 2:
-            //  Bir dənə anket proqramı yığacaqsınız.İstifadəçidən onun haqqında məlumatları toplayacaqsınız.Bu məlumatlar ad, soyad, cins, doğum tarixi,
-            //yaşadığı ölkə, yaşadığı şəhər, boy, çəki, maaş olacaq.Siz bu məlumatları saxlamaq üçün doğru bir data type seçməlisiniz.
-            //  İstifadəçinin daxil edəcəyi məlumatlar belə olacaq:
-            //  Ad: Mark
-            //  Soyad: Henry
-            //  Cins: K və ya Q
-            //  Doğum tarixi: 1990 - 10 - 10
-            //  Yaşadığı ölkə: Azərbaycan
-            //  Yaşadığı şəhər: Bakı
-            //  Boy: 1.75
-            //  Çəki: 26.50
-            //  Maaş: 2500.00
-
-            {
-                string name, surname, country, city;
-                bool gender;
-                DateTime born;
-                float height, weight;
-                    decimal salary;
-
-                Console.WriteLine("Please enter your name");
-                name = Console.ReadLine();
-
-                Console.WriteLine("Please enter your surname");
-                surname = Console.ReadLine();
-
-                Console.WriteLine("Please enter your gender (male/female)");
-                gender = Console.ReadLine().ToLower() == "female" ? Convert.ToBoolean(0) : Convert.ToBoolean(1);
-
-                Console.WriteLine("Please enter date of birth  ex. year/month/day");
-                born = Convert.ToDateTime(Console.ReadLine());
-
-                Console.WriteLine("Please enter country you live");
-                country = Console.ReadLine();
-
-                Console.WriteLine("Please enter city you live");
-                city = Console.ReadLine();
-
-                Console.WriteLine("Please enter your height");
-                height = (float)Convert.ToDouble(Console.ReadLine());
-
-                Console.WriteLine("Please enter your weight");
-                weight = (float)Convert.ToDouble(Console.ReadLine());
-
-                Console.WriteLine("Please enter your salary");
-                salary = Convert.ToDecimal(Console.ReadLine());
-
-                Console.ReadLine();
-            }
+            char stringToChar = Convert.ToChar(st1);
+            int stringToInt = Convert.ToInt32(st);
+            string intToString = Convert.ToString(b);
+            byte charToByte = Convert.ToByte(c);
 
 
+            //Task 2
+            string name, surname, country, city;
+            float height, weight;
+            decimal salary;
+            int age;
+            DateTime dateOfBirth;
+            bool sex;
 
-            //Homework 3:
-            //İstifadəçidən yaşını, maaşını, boyunu, çəkisini və neçə dostu olduğunu soruşacaqsınız.Daxil etdiyi bütün dəyərləri ilk öncə int tipinə convert
-            //edəcəksiniz.Daha sonra int dəyişənini byte tipinə cast edəcəksiniz(explicit conversion).
-            //Burada istifadəçi maaşı və yaşı xaricində digər məlumatları byte'ın max dəyərindən çox yazsa heç bir xəta çıxmasın. Maaşını və ya yaşını byte
-            //tipinin qəbul edəcəyi maksimum dəyərdən çox yazarsa elə edin ki, proqramda xəta çıxsın.
-
-            int age, salary, height, weight;
-
-            Console.WriteLine("Please insert your");
-            Console.WriteLine("Age");
-            age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Salary");
-            salary = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter your");
+            Console.WriteLine("Name");
+            name = Console.ReadLine();
+            Console.WriteLine("Surname");
+            surname = Console.ReadLine();
+            Console.WriteLine("Sex");
+            sex = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Date of birth ex. year/month/day");
+            dateOfBirth = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Country");
+            country = Console.ReadLine();
+            Console.WriteLine("City");
+            city = Console.ReadLine();
             Console.WriteLine("Height");
-            height = Convert.ToInt32(Console.ReadLine());
+            height = float.Parse(Console.ReadLine());
             Console.WriteLine("Weight");
-            weight = Convert.ToInt32(Console.ReadLine());
+            height = float.Parse(Console.ReadLine());
+            Console.WriteLine("Salary ");
+            salary = Convert.ToDecimal(Console.ReadLine());
 
+            //Task 3
+            Console.WriteLine("Enter your age");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your salary");
+            salary = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your height");
+            height = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your weight");
+            weight = float.Parse(Console.ReadLine());
 
+            int salaryToInt = (int)salary;
+            int heightToInt = (int)height;
+            int weightToInt = (int)weight;
 
-            //Homework 4:
-            //Aşağıdakı dəyərləri düzgün tiplərdə dəyişənlər yaradıb saxlayın:
-            //1: "Hava gozeldir." -> string
-            //2: '%' -> char
-            //3: 9999999999999999 -> long
-            //4: true -> bool
-            //5: false -> bool
-            //6: '0' -> char
-            //7: 0 -> byte
-            //8: 150 -> byte
-            //9: -111111111115615 -> slong
-            //10: 15.2M -> decimal
-            //11: 15.2 -> double
-            //12: 15.2F -> float
-            //13: new DateTime(2020, 12, 12) -> DateTime
-
+            byte heightToByte = (byte)heightToInt; //xetasiz
+            byte weightToByte = (byte)weightToInt; //xetasiz
+            byte salaryToByte = Convert.ToByte(salaryToInt); //xeta cixacaq salary 255-den boyuk olarsa
+            byte agteToByte = Convert.ToByte(age); //xeta cixacaq yash 255-den boyuk olarsa
         }
-}
+    }
 }
